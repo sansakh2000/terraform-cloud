eb6c227c3e2ecb32b105c31132326a4ea609fc63
Santosh Sakhare <sansakh2000@gmail.com>
2022-07-15 19:14:44 +0530

    add provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+
 provider "aws" {
   region = var.region
 }
